73b10a661f851d824de29313a6022d139ed79ee5
BBogle0519 <leetmdgud@gmail.com>
2021-11-03 21:41:06 +0900

    21.11.03
    local 환경으로 변경하여 구성
    android <-> django <-> mysql
    데이터 이동 확인

--- app/src/main/java/com/example/sample/RetrofitClient.java
+++ app/src/main/java/com/example/sample/RetrofitClient.java
@@ -4,7 +4,10 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private final static String BASE_URL = "http://3.36.135.85:8000";
+    // localhost(에뮬레이터에선 10.0.2.2로 사용)
+    private final static String BASE_URL = "http://10.0.2.2:8000";
+    // AWS 주소
+    // private final static String BASE_URL = "http://3.36.135.85:8000";
     private static Retrofit retrofit = null;
 
     private RetrofitClient() {
