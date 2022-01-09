6804fddb9b5584c791a745b330303722b46d774b
BBogle0519 <leetmdgud@gmail.com>
2022-01-10 04:42:21 +0900

    사용자 선택에 따라 일별 월별 년도별 걸음수 통계 그래프 표현 완료

--- app/src/main/java/com/example/sample/Statistics.java
+++ app/src/main/java/com/example/sample/Statistics.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,7 +17,7 @@ import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.gson.Gson;
 
@@ -31,31 +33,44 @@ public class Statistics extends AppCompatActivity {
     int user_id_pk = 0;
 
     BarChart barchart;
+    Spinner spinner;
     ArrayList<String> label = new ArrayList<>();
     ArrayList<Integer> chart_data = new ArrayList<>();
-
-    ArrayList<String> year = new ArrayList<>();
-    ArrayList<String> month = new ArrayList<>();
-    ArrayList<String> day = new ArrayList<>();
-    ArrayList<Integer> step = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistics);
+
+        //retrofit2
         service = RetrofitClient.getClient().create(ApiService.class);
 
         Intent intent = getIntent();
         user_id_pk = intent.getIntExtra("user_id_pk", 0);
         //Log.e("Statistics\n", "user_id_pk: " + user_id_pk);
-        getStatistics(new StatisticsData(user_id_pk));
+        //getStatistics(new StatisticsData(user_id_pk));
 
         // 막대 그래프
         barchart = findViewById(R.id.barchart);
+
+        // 스피너(콤보박스)
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                getStatistics(new StatisticsData(user_id_pk), i);
+                //Toast.makeText(getApplicationContext(), adapterView.getItemAtPosition(i) +"이 선택되었습니다. " + i, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //Toast.makeText(getApplicationContext(), "onNothingSelected", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
-    private void getStatistics(StatisticsData data) {
+    private void getStatistics(StatisticsData data, int i) {
         service.step_statistics(data).enqueue(new Callback<StatisticsResponse>() {
             @Override
             public void onResponse(@NonNull Call<StatisticsResponse> call, @NonNull Response<StatisticsResponse> response) {
@@ -65,38 +80,39 @@ public class Statistics extends AppCompatActivity {
 
                 if (response.isSuccessful()) {
                     Log.e("stepRecord 2xx\n", "response.code(): " + response.code());
-                    String message = result.getMessage();
+
                     List<StatisticsItem> year_data = result.getYear_data();
                     List<StatisticsItem> month_data = result.getMonth_data();
                     List<StatisticsItem> day_data = result.getDay_data();
 
-//                    if (사용자선택 == 연도별) {
-//
-//                    } else if (사용자선택 == 월별) {
-//
-//                    } else if (사용자선택 == 일별) {
-//
-//                    }
+                    if (i == 0) { //일별
+                        for (int i = 0; i < day_data.size(); i++) {
+                            label.add(i, day_data.get(i).getYear() + "/" + day_data.get(i).getMonth() + "/" + day_data.get(i).getDay());
+                            chart_data.add(i, day_data.get(i).getStep());
+                        }
 
-                    for (int i = 0; i < year_data.size(); i++) {
-                        label.add(i, year_data.get(i).getYear());
-                        chart_data.add(i, year_data.get(i).getStep());
+                    } else if (i == 1) { //월별
+                        for (int i = 0; i < month_data.size(); i++) {
+                            label.add(i, month_data.get(i).getYear() + "/" + month_data.get(i).getMonth());
+                            chart_data.add(i, month_data.get(i).getStep());
+                        }
+
+                    } else if (i == 2) { //연도별
+                        for (int i = 0; i < year_data.size(); i++) {
+                            label.add(i, year_data.get(i).getYear());
+                            chart_data.add(i, year_data.get(i).getStep());
+                        }
                     }
+
                     // 그래프 세팅
                     setBarchart(label, chart_data);
 
-//                    for (int i = 0; i < month_data.size(); i++) {
-//                        year.add(i, month_data.get(i).getYear());
-//                        month.add(i, month_data.get(i).getMonth());
-//                        step.add(i, month_data.get(i).getStep());
+//                    for (int i = 0; i < year_data.size(); i++) {
+//                        label.add(i, year_data.get(i).getYear());
+//                        chart_data.add(i, year_data.get(i).getStep());
 //                    }
-//
-//                    for (int i = 0; i < day_data.size(); i++) {
-//                        year.add(i, day_data.get(i).getYear());
-//                        month.add(i, day_data.get(i).getMonth());
-//                        day.add(i, day_data.get(i).getDay());
-//                        step.add(i, day_data.get(i).getStep());
-//                    }
+//                    // 그래프 세팅
+//                    setBarchart(label, chart_data);
 
                 } else {
                     // 저장된 데이터 없음
@@ -115,22 +131,16 @@ public class Statistics extends AppCompatActivity {
         ArrayList<BarEntry> entries = new ArrayList<>();
 
         for (int i = 0; i < data_list.size(); i++) {
-            entries.add(new BarEntry(Integer.parseInt(label_list.get(i)), data_list.get(i)));
+            entries.add(new BarEntry(i, data_list.get(i)));
         }
 
         BarDataSet dataSet = new BarDataSet(entries, "steps");
-        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        dataSet.setColors(Color.GREEN);
         dataSet.setValueTextColor(Color.BLACK);
         BarData data = new BarData(dataSet);
         data.setBarWidth(0.3f);
-        
-        // 그래프 설정
-        barchart.setMaxVisibleValueCount(7);
-        barchart.setFitBars(true);
+        // 데이터 세팅
         barchart.setData(data);
-        barchart.getDescription().setText("year step count");
-        barchart.animateY(2000);
-        barchart.setTouchEnabled(false);
 
         // x축
         barchart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
@@ -141,6 +151,18 @@ public class Statistics extends AppCompatActivity {
         barchart.getAxisLeft().setDrawAxisLine(false);
         barchart.getAxisLeft().setDrawGridLines(false);
 
+        // 그래프 설정
+        barchart.setMaxVisibleValueCount(7);
+        barchart.setFitBars(true);
+        barchart.getDescription().setText("year step count");
+        barchart.animateY(2000);
+        barchart.setTouchEnabled(false);
+        barchart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(label_list));
+        barchart.invalidate();
+
+        // ArrayList 초기화
+        label_list.clear();
+        data_list.clear();
     }
 
 }
